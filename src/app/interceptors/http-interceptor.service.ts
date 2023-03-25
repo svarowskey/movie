9fe16d426a9f8eb163da2f768f8c880ab8d069dc
Vladimir <vladimir.svarowski@gmail.com>
2023-03-25 10:42:19 +0600

    disable caching in http-interceptor

--- src/app/interceptors/http-interceptor.service.ts
+++ src/app/interceptors/http-interceptor.service.ts
@@ -3,33 +3,16 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor, HttpResponse
+  HttpInterceptor
 } from '@angular/common/http';
-import { Observable, of, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class MovieInterceptor implements HttpInterceptor {
 
-  private cache = new Map<string, any>();
-
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if (request.method !== 'GET') {
-      return next.handle(request);
-    }
-
-    const cachedResponse = this.cache.get(request.url);
-    if (cachedResponse) {
-      return of(cachedResponse);
-    }
-
-    return next.handle(request).pipe(
-      tap(event => {
-        if (event instanceof HttpResponse) {
-          this.cache.set(request.url, event);
-        }
-      })
-    );
+    return next.handle(request);
   }
 }
