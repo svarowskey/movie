006b7ca3f63742c7019670b0a6b1d4f78cdaf5d6
Vladimir <vladimir.svarowski@gmail.com>
2023-04-05 23:24:46 +0600

    Add search-field and last changes

--- src/app/components/shared-components.module.ts
+++ src/app/components/shared-components.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IconComponent } from "./icon/icon.component";
 import { ButtonAddComponent } from './buttons/button-add/button-add.component';
 import { ButtonWatchNowComponent } from './buttons/button-watch-now/button-watch-now.component';
+import { SearchFieldComponent } from './search-field/search-field.component';
 
 
 
@@ -10,7 +11,8 @@ import { ButtonWatchNowComponent } from './buttons/button-watch-now/button-watch
   declarations: [
     IconComponent,
     ButtonAddComponent,
-    ButtonWatchNowComponent
+    ButtonWatchNowComponent,
+    SearchFieldComponent
   ],
   imports: [
     CommonModule
@@ -18,7 +20,8 @@ import { ButtonWatchNowComponent } from './buttons/button-watch-now/button-watch
     exports: [
         IconComponent,
         ButtonAddComponent,
-        ButtonWatchNowComponent
+        ButtonWatchNowComponent,
+        SearchFieldComponent
     ]
 })
 export class SharedComponentsModule { }
