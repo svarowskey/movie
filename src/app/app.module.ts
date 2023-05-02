2eab2688b3feab6b77729e7f0e8087b3dec7921c
Vladimir <vladimir.svarowski@gmail.com>
2023-05-02 19:22:38 +0600

    Add taiga ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -7,7 +10,8 @@ import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, BrowserAnimationsModule, TuiRootModule, TuiDialogModule, TuiAlertModule],
   bootstrap: [AppComponent],
-})
+        providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
+    })
 export class AppModule {}
